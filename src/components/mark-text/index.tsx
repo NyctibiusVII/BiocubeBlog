7bbffe93c4b84938edbc71686c62c85c2a671911
NyctibiusVII <matheus.dev.07@gmail.com>
2024-04-29 20:28:49 -0300

    feat: Adding underline in MarkText component

--- src/components/mark-text/index.tsx
+++ src/components/mark-text/index.tsx
@@ -1,26 +1,29 @@
 import React from 'react'
 
-type MarkTextColorsType = 'transparent' | 'white' | 'green' | 'yellow' | 'orange' | 'red' | 'purple' | 'blue' | 'black'
+type MarkTextColorsType = 'transparent' | 'current' | 'white' | 'green' | 'yellow' | 'orange' | 'red' | 'purple' | 'blue' | 'black'
 interface MarkTextProps extends React.HTMLAttributes<HTMLSpanElement> {
     color?: MarkTextColorsType
+    underline?: boolean
 }
 
 /**
- * MarkText - Component to highlight text with a specific color.
+ * MarkText - Component to highlight text with a specific color and underline it.
  *
  * @param {React.ReactNode} children
  * @param {string} color
+ * @param {boolean} underline
  * @returns {JSX.Element}
  * @example
- * <MarkText color='green'>Hello World</MarkText>
+ * <MarkText color='green' underline>Hello World</MarkText>
  *
- * return <span className='text-green'>Hello World</span>
+ * return <span className='text-green underline'>Hello World</span>
  **/
-export const MarkText = ({ color='green', ...spanProps }: MarkTextProps): JSX.Element => {
+export const MarkText = ({ color='green', underline=false, ...spanProps }: MarkTextProps): JSX.Element => {
     const { className, ...props } = spanProps
 
     const colorClass: Record<MarkTextColorsType, string> = {
         'transparent': 'text-transparent',
+        'current': 'text-current',
         'white': 'text-white',
         'green': 'text-leafgreen',
         'yellow': 'text-moonmist',
@@ -31,9 +34,11 @@ export const MarkText = ({ color='green', ...spanProps }: MarkTextProps): JSX.El
         'black': 'text-black'
     }
 
+    const underlineClass = underline ? 'underline' : ''
+
     let classNameList = ''
-    if (className) classNameList = [colorClass[color], className].join(' ')
-    else classNameList = colorClass[color]
+    if (className) classNameList = [colorClass[color], underlineClass, className].join(' ')
+    else classNameList = [colorClass[color], underlineClass].join(' ')
 
     return <span className={classNameList} {...props} />
 }
