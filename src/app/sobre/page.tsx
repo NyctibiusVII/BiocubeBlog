df711c09834d943d1caa9c067ddc31fa24771350
NyctibiusVII <matheus.dev.07@gmail.com>
2024-04-29 20:31:19 -0300

    feat: Adding text about the company

--- src/app/sobre/page.tsx
+++ src/app/sobre/page.tsx
@@ -35,6 +35,40 @@ export default function About() {
                         <p className={`${BigShouldersText.className} text-right text-2xl xs:text-4xl sm:text-[3.25rem] sm:leading-[3.5rem] md:text-[2.5rem] md:leading-[2.75rem] lg:text-6xl xl:text-[4.75rem] xl:leading-[5.25rem] pr-2 xl:pr-4`}><MarkText>CEO</MarkText> & <MarkText>DESENVOLVEDOR</MarkText></p>
                     </div>
                 </div>
+
+                <div className='w-full flex flex-col sm:flex-row md:flex-col lg:flex-row gap-16 sm:gap-8 md:gap-16 lg:gap-12'>
+                    <div className='w-full flex flex-col items-center sm:items-start gap-4'>
+                        <h2 className={`w-full ${BigShouldersText.className} uppercase text-2xl xs:text-3xl sm:text-4xl lg:text-5xl`}>O Que Fazemos</h2>
+                        <p className={`w-full ${BigShouldersText.className} uppercase text-wrap leading-5 xs:text-lg xs:leading-[1.375rem] sm:text-xl sm:leading-6 lg:text-2xl lg:leading-7`}>
+                            A <MarkText>Biocube</MarkText> foi criada com a missão de levar aos cubistas brasileiros a mesma <MarkText color='current' underline>qualidade e desempenho</MarkText> nos cubos que os principais competidores mundiais utilizam. <MarkText>Magnetizamos</MarkText> cubos, <MarkText>ajustamos</MarkText> e a <MarkText>lubrificação</MarkText> é feita com Lubes específicos para cubo mágico <MarkText color='current' underline>produzidos em laboratório</MarkText> ou <MarkText color='current' underline>importados</MarkText> e de qualidade superior a qualquer tipo de spray ou lubrificantes de silicone comum. <br/><br/>
+                            Este serviço é sempre feito com <MarkText color='current' underline>extremo cuidado</MarkText> e <MarkText color='current' underline>perfeccionismo</MarkText>, <MarkText>evitando assim os lapsos</MarkText> cometidos na produção em massa que muitas vezes deixam a desejar na qualidade e finalização dos cubos que chegam sempre desregulados e secos internamente, fazendo com que o puzzle se desgaste rapidamente e perca seu real desempenho.
+                        </p>
+                        <Image
+                            className='w-64 lg:w-80 xl:w-[22.438rem] border-2 border-leafgreen/50 rounded-md lg:rounded-lg mt-2'
+                            src='/assets/diversified-cubes.png'
+                            width={359}
+                            height={180}
+                            priority={false}
+                            alt='Foto de cubos diversificados da Biocube.'
+                        />
+                    </div>
+
+                    <div className='w-full flex flex-col items-center sm:items-end gap-4'>
+                        <h2 className={`w-full ${BigShouldersText.className} text-right uppercase text-2xl xs:text-3xl sm:text-4xl lg:text-5xl`}>Nosso Trabalho</h2>
+                        <p className={`w-full ${BigShouldersText.className} text-right uppercase text-wrap leading-5 xs:text-lg xs:leading-[1.375rem] sm:text-xl sm:leading-6 lg:text-2xl lg:leading-7`}>
+                            Atualmente estamos trabalhando com <MarkText color='current' underline>diversos tipos de cubos</MarkText> fazendo a lubrificação total do quebra cabeça e ajustes de cada peça, nos <MarkText>Cubos</MarkText> (2x2; 3x3; 4x4; 5x5; 6x6 e 7x7), <MarkText>Pyraminx</MarkText>, <MarkText>Megaminx</MarkText>, <MarkText>Square1</MarkText> e <MarkText>Skewb</MarkText>, nós também modificamos alguns modelos.<br/>Trabalhamos com cubos <MarkText color='current' underline>não oficiais</MarkText> e <MarkText color='current' underline>Edições Limitadas</MarkText> também.<br/><br/>
+                            Alguns de nossos cubos são entregues com um saquinho para carregar o cubo e evitar arranhar na mochila ou na bolsa.
+                        </p>
+                        <Image
+                            className='w-64 lg:w-80 xl:w-[22.438rem] border-2 border-leafgreen/50 rounded-md lg:rounded-lg mt-2'
+                            src='/assets/tiago-s-studio.png'
+                            width={359}
+                            height={180}
+                            priority={false}
+                            alt='Foto do studio do Tiago, CEO da Biocube.'
+                        />
+                    </div>
+                </div>
             </main>
         </>
     )
